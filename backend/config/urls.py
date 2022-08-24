8f91c492cef793e408e9e8e561e7306204e1f074
Maksimov <amaksimov744@gmail.com>
2022-08-24 16:45:19 +0300

    Добрый день. Выполнены все задания.

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,12 +17,20 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path, include
-
+from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('usersapp.urls', namespace='users')),
     path('api/todo/', include('todoapp.urls', namespace='todo')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api-auth-token/', views.obtain_auth_token),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 if settings.DEBUG:
