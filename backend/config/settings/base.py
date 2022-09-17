17eace3629f9e643b20a48a3fd13924eda19a8d8
Maksimov <amaksimov744@gmail.com>
2022-09-17 16:52:09 +0300

    Добрый день. Выполнены обязательные задания.

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -13,7 +13,7 @@ import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-%z51lrjry328-0vo60ym5a7v!d(vx4i&(!t7q8a3#-05(l^y37
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
